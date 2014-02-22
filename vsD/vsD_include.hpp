11946c0c445500f4aa8745c284a3c6650a71cff9
verbalsaintmars <11008huan.tech@gmail.com>
2014-02-22 11:06:39 -0800

    vsD

---
+++ vsD/vsD_include.hpp
@@ -0,0 +1,13 @@
+/* ******************************
+ * Copyleft 2014 Verbalsaint
+ * ******************************/
+
+#ifndef _VSD_INCLUDE_HPP
+#define _VSD_INCLUDE_HPP
+#include <daemon.hpp>
+#include <signal.hpp>
+#include <disposition.hpp>
+
+extern "C" int errno;
+
+#endif // for #ifndef _VSD_INCLUDE_HPP
