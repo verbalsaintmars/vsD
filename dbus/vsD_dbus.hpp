f8ceef5a58ae31704cb23f329ce9d28f71700761
verbalsaintmars <11008huan.tech@gmail.com>
2014-02-22 11:04:40 -0800

    dbus module

---
+++ dbus/vsD_dbus.hpp
@@ -0,0 +1,9 @@
+/* ******************************
+ * Copyleft 2014 Verbalsaint
+ * ******************************/
+
+#ifndef _VSD_DBUS_HPP
+#define _VSD_DBUS_HPP
+
+
+#endif // for #ifndef _VSD_DBUS_HPP
